c78f27f5b4d9170dc2fbb654e276d3b2dd37e6a6
Joshua Hwang <joshchwang@gmail.com>
2016-02-17 13:40:11 -0800

    organized templates

--- gulpfile.js
+++ gulpfile.js
@@ -13,21 +13,21 @@ gulp.task('nodemon', function() {
 
 gulp.task('jade', function() {
   console.log('Running jade templating..');
-  gulp.src('./jade/**/*.jade')
+  gulp.src('./template/jade/**/*.jade')
    .pipe(jade())
    .pipe(gulp.dest('./app/public/views'))
   });
 
 gulp.task('sass', function() {
   console.log('Running sass templating..');
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./template/sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./app/public/css'))
   });
 
 gulp.task('watch', function() {
-  gulp.watch('./jade/**/*.jade', ['jade']);
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./template/jade/**/*.jade', ['jade']);
+  gulp.watch('./template/sass/**/*.scss', ['sass']);
   });
 
 gulp.task('default', ['jade', 'sass', 'watch', 'nodemon']);

e07eafc493845dc5b21782cec1b91bdd484c14d6
Joshua Hwang <joshchwang@gmail.com>
2016-02-17 17:11:16 -0800

    (#1) added cookies for log in

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var app        = express();
 var path       = require('path');
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+var sessions   = require('client-sessions');
 
 mongoose.connect('mongodb://localhost/status');
 
@@ -24,14 +25,22 @@ app.locals.pretty = true;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(sessions({
+  cookieName:     'session',
+  secret:         'fhasd234fsebv267823r23',
+  duration:       30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000
+}));
+
 app.use(express.static('public'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// Routes
+//-- Routes
 app.get('/', function(req, res) {
   res.render('../template/jade/index.jade');
 });
 
+// Registration
 app.get('/register', function(req, res) {
   res.render('../template/jade/register.jade');
 });
@@ -58,6 +67,7 @@ app.post('/register', function(req, res) {
   });
 });
 
+// Login
 app.get('/login', function(req, res) {
   res.render('../template/jade/login.jade');
 });
@@ -69,6 +79,7 @@ app.post('/login', function(req, res) {
       console.log('Invalid email or password.');
     } else {
       if (req.body.password === user.password && req.body.medId === user.medId) {
+        req.session.user = user;
         res.redirect('/dashboard');
       } else {
         res.render('../template/jade/login.jade', { error: 'Invalid email or password.' });
@@ -79,10 +90,22 @@ app.post('/login', function(req, res) {
 });
 
 app.get('/dashboard', function(req, res) {
-  res.render('../template/jade/dashboard.jade');
+  if (req.session && req.session.user) {
+    User.findOne({ email: req.session.user.email }, function(err, user) {
+      if (!user) {
+        req.session.reset(); // Reset the session
+        res.redirect('/login');
+      } else {
+        res.render('../template/jade/dashboard.jade');
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get('/logout', function(req, res) {
+  req.session.reset(); // Reset the session
   res.redirect('/');
 });
 

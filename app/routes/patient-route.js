be8d3f4fe92dbe36fba4d93924c99b06cf5c10cd
Joshua Hwang <joshchwang@gmail.com>
2016-02-24 22:31:49 -0800

    (#3) added route and save function for patients

---
+++ app/routes/patient-route.js
@@ -0,0 +1,33 @@
+var express    = require('express');
+var path       = require('path');
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var sessions   = require('client-sessions');
+var Patient    = require('./patients.js');
+
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+
+//-- Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//-- Routes
+app.post('/patients', function(req, res) {
+  var patient = new Patient({
+    firstName: req.body.firstName,
+    lastName:  req.body.firstName,
+    patientId: req.body.patientId,
+    profile:   req.body.profile,
+    drugs:     req.body.drugs,
+    labs:      req.body.labs,
+  });
+
+  patient.save(function(err) {
+    if (err) {
+      var err = 'Something bad happened, try again!';
+      res.render('../template/jade/dashbaord.jade', { error: error });
+    } else {
+      res.redirect('/dashboard');
+    }
+  })
+});
